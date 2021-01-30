6f35343c661244e05e829eba948eb06323ecb4d3
Eliasstl <ejmotoboy1@gmail.comn>
2021-01-30 13:18:42 -0200

    Index.js Editado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import Header from './components/Header'
-import'./style.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+import Header from './components/Header';
+import Feed from './components/Feed';
+import NavBar from './components/NavBar';
+
+const usesStyles = makeStyles({
+    root:{
+        display:'flex',
+        flexDirection:'column'
+    },
+    main:{
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+
+})
 
 function Home(){
+    const classes = usesStyles();
     return(
-        <div>
+        <div className={classes.root}>
             <Header/>
-            <main className='main'>
-                <div className='navbar'>navbar</div>
-                <div className='feed'> feed</div>
+            <div className="toolbar"></div>
+            <main className={classes.main}>
+               <NavBar/>
+                <Feed/>
             </main>
-           
-
         </div>
     );
    
