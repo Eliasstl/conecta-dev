07222f8a2b2f03c0b91f1486865323ac92387e7b
Eliasstl <ejmotoboy1@gmail.comn>
2021-01-30 16:19:22 -0200

    NavBar Completo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Feed from './components/Feed';
 import NavBar from './components/NavBar';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 const usesStyles = makeStyles({
     root:{
@@ -13,22 +15,29 @@ const usesStyles = makeStyles({
     },
     main:{
         height: '100vh',
-        display: 'flex',
-        width: '1200px',
-        margin: '0 auto',
+        padding: 24,
+   
+    },
+    toolbar:{
+        minHeight: 64,
     }
 
-})
+});
 
 function Home(){
     const classes = usesStyles();
     return(
         <div className={classes.root}>
             <Header/>
-            <div className="toolbar"></div>
+            <div className={classes.toolbar}></div>
             <main className={classes.main}>
-               <NavBar/>
-                <Feed/>
+                <Container maxWidth="lg">
+                    <Box display="flex">
+                        <NavBar/>
+                        <Feed/>
+                    </Box>
+                </Container>
+               
             </main>
         </div>
     );
